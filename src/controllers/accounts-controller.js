39834011a5938d6f76ed71ef22430963f1cb8295
cmddev-admin <daly.caroline@live.com>
2023-05-26 08:14:23 -0600

    security updates - hashing and salting passwords

--- src/controllers/accounts-controller.js
+++ src/controllers/accounts-controller.js
@@ -2,9 +2,11 @@
  * Accounts Controller within the application
  * @Author: Caroline Daly
  */
-
+import bcrypt from "bcrypt";
 import { UserSpec, UserCredentialsSpec } from "../models/joi-schemas.js";
 import { db } from "../models/db.js";
+
+const saltRounds = 10;
 
 export const accountsController = {
   index: {
@@ -30,6 +32,7 @@ export const accountsController = {
     },
     handler: async function (request, h) {
       const user = request.payload;
+      user.password = await bcrypt.hash(user.password, saltRounds); 
       await db.userStore.addUser(user);
       return h.redirect("/");
     },
@@ -52,7 +55,9 @@ export const accountsController = {
     handler: async function (request, h) {
       const { email, password } = request.payload;
       const user = await db.userStore.getUserByEmail(email);
-      if (!user || user.password !== password) {
+      const passwordsMatch = await bcrypt.compare(password, user.password);   
+      if (!user || !passwordsMatch) { 
+      // if (!user || user.password !== password) {
         return h.redirect("/");
       }
       request.cookieAuth.set({ id: user._id });
