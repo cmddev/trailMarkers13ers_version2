f3067f105e1604b2158b6aa20701e8804ebd4f7b
cmddev-admin <daly.caroline@live.com>
2023-05-27 16:13:46 -0600

    set up cypress testing

---
+++ cypress/e2e/testingCypressSetUpspec.cy.js
@@ -0,0 +1,18 @@
+describe("My First Test", () => {
+  it("Does not do much!", () => {
+    expect(true).to.equal(true)
+  })
+})
+
+describe("My First Test", () => {
+  it("Does not do much!", () => {
+    expect(true).to.equal(false)
+  })
+})
+
+describe("My First Test", () => {
+  it("Visit Login Page!", () => {
+    cy.visit("localhost:3000/login")
+    // need to update once deployed
+  })
+})
